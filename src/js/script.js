e5c3f42177f4464360d5b53395e7bc683c06cb70
Krystian Otto <otto.krystiann@gmail.com>
2020-07-02 20:57:06 +0200

    fix dirs, add first class -> script.js

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,27 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(){
+      const thisProduct = this;
+      console.log('This procdoct = ', thisProduct);
+    }
+  }
+
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+      console.log('thisApp data', thisApp.data);
+
+      const testProduct = new Product();
+      console.log('test product ', testProduct);
+    },
+
+    initData: function (){
+      const thisApp = this;
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,6 +80,9 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      app.initMenu();
     },
   };
 
